8296f04a257cd6b4ad704cd1cae1859f7b9ef0ee
Aleksandar <sale.novakovic403@gmail.com>
2019-03-18 19:21:07 +0100

    Asynchronous connection
    Asynchronous connection between brother instances via internalEndPoint and Tasks

--- v3/InterroleCommunication/JobWorker/JobServerProvider.cs
+++ v3/InterroleCommunication/JobWorker/JobServerProvider.cs
@@ -1,8 +1,10 @@
 ﻿using InterroleContracts;
+using Microsoft.WindowsAzure.ServiceRuntime;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,19 +12,60 @@ namespace JobWorker
 {
     public class JobServerProvider : IJob
     {
+        private ServiceHost serviceHost;
+        private string internalEndpointName = "InternalRequest";
+        NetTcpBinding binding = new NetTcpBinding();
+
         public int DoCalculus(int to)
         {
+            //2.
             //potrebno je izračunati sumu i ispisati poruku u Compute emulatoru: “DoCalculus
             //method called - interval[1, n]”
 
+            //3.
+            //Metodu IJob interfejsa kog implementira JobServerProvider izmeniti tako da osim
+            //ispisivanja intervala u compute emulatoru poziva metode IPartialJob interfejsa ostalih instanci
+            //prosleđujući iste vrednosti intervala.
+
             Trace.WriteLine($"DoCalculus method called - interval [1, {to}");
-            int rez = 0;
-            for(int i=0; i<=to; i++)
+
+            // all internal endpoints of all worker role processes not including this
+            //worker role process:
+            List<EndpointAddress> internalEndPoints =
+                RoleEnvironment.Roles[RoleEnvironment.CurrentRoleInstance.Role.Name].
+                Instances.Where(i => i.Id != RoleEnvironment.CurrentRoleInstance.Id).
+                Select(process => new EndpointAddress(String.Format("net.tcp://{0}/{1}",
+                process.InstanceEndpoints[internalEndpointName].IPEndpoint.ToString(),
+                internalEndpointName))).ToList();
+
+            int brotherInstances = internalEndPoints.Count;
+            int totalSum = 0;
+
+            Task<int>[] tasks = new Task<int>[brotherInstances];
+
+            for(int i=0; i< brotherInstances; i++)
             {
-                rez += i;
+                Trace.WriteLine($"Calling node at: {internalEndPoints[i].ToString()}", "Information");
+                int index = i;
+
+                Task<int> calculatePartialSum = new Task<int>(() =>
+                    {
+                        IPartialJob proxy = new ChannelFactory<IPartialJob>(binding, internalEndPoints[index]).CreateChannel();
+                        return proxy.DoSum(0, to);
+                    });
+                calculatePartialSum.Start();
+
+                tasks[index] = calculatePartialSum;
             }
 
-            return rez;
+            Task.WaitAll(tasks);
+
+            foreach(Task<int> task in tasks)
+            {
+                totalSum += task.Result;
+            }
+
+            return totalSum;
         }
     }
 }
---
+++ v3/InterroleCommunication/JobWorker/PartialJobServerProvider.cs
@@ -0,0 +1,29 @@
+﻿using InterroleContracts;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobWorker
+{
+    public class PartialJobServerProvider : IPartialJob
+    {
+        public int DoSum(int from, int to)
+        {
+            Trace.WriteLine($"DoSum method called - interval [{from}, {to}]");
+            //TODO:
+            //Implementirati algoritam sukcesivnog sabiranja i vraćati
+            //vrednost kao odgovor metode DoSum.
+
+            int result = 0;
+            for (int i = from; i <= to; i++)
+            {
+                result += i;
+            }
+            return result;
+
+        }
+    }
+}

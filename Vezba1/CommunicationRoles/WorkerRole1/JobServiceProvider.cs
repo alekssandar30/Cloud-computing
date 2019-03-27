bb12573946e3dd166b89c6d215f2e4e9fecf1480
Aleksandar <sale.novakovic403@gmail.com>
2019-03-27 14:59:12 +0100

    Init

---
+++ Vezba1/CommunicationRoles/WorkerRole1/JobServiceProvider.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerRole1
+{
+    public class JobServiceProvider : IForward
+    {
+        public string Send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
